98a639927e9d22108f978a64619b7ee704f195a5
Guido <119985142+guidoxenofonte2005@users.noreply.github.com>
2024-09-24 12:29:27 -0300

    locuras

--- .venv/gameScripts/dialogueView.py
+++ .venv/gameScripts/dialogueView.py
@@ -1,7 +1,9 @@
 
+# classe pra lidar com diálogos
 class DialogueView:
-    def __init__(self, lines, dialogueBoxSprite = None) -> None:
-        pass
+    def __init__(self, lines, npc, dialogueBoxSprite = None) -> None:
+        self.lines = lines
+        self.npc = npc
 
     def update(self):
         pass
