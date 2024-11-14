1f53eb7e69c59d7bfe41b18f8af59f41a4955c65
Guido <119985142+guidoxenofonte2005@users.noreply.github.com>
2024-11-14 00:17:46 -0300

    main menu now working completely, started implementing quiz system

--- .venv/game.py
+++ .venv/game.py
@@ -32,6 +32,7 @@ class Game:
         self.assets : dict = {
             'player/idle' : Animation(load_images('characters/filip/idle'), 18),
             'player/walk' : Animation(load_images('characters/filip/walk'), 12),
+            'player/suit' : Animation(load_images('characters/filip/suit'), 18),
             'debugCat' : Animation(load_images('animals/cat1'), 8),
             'debugCat2' : Animation(load_images('animals/cat2')),
             'marble' : load_images('tiles/'),
@@ -58,7 +59,7 @@ class Game:
 
         self.currentLevel : str
         self.levelVar : int
-        self.transition : bool = False
+        self.transition : int = 0
 
         self.interactableObjects = [
             InteractiveObject((10, 245), 37, ["dialogue"], self, 'debugCat'),
@@ -124,10 +125,9 @@ class Game:
         for key in list(self.buttonsOnScreen.keys()):
             self.buttonsOnScreen[key].kill()
             del self.buttonsOnScreen[key]
-        
+
         self.transition = -30
         self._runTransition_()
-            
 
     def loadLevel(self, pastLevelId : int, direction : int, customDirection : int = 0) -> None:
         match direction:
@@ -138,89 +138,146 @@ class Game:
             case _:
                 self.tilemap.load(f".venv/maps/{self.currentLevel}{str(pastLevelId + customDirection)}.json")
         self.transition = -30
-        
+
+    def __loadCustomLevel__(self, levelName):
+        self.tilemap.load(f".venv/maps/{levelName}.json")
+        self.transition = -30
 
     def run(self):
         print(self.buttonsOnScreen)
         while True:
-            time_delta = self.clock.tick(60)/1000.0
-            self.display.fill((28, 138, 217))
-
-            self.scroll[0] += (self.Player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 20
-            self.scroll[1] += (self.Player.rect().centery - self.display.get_height() / 1.6 - self.scroll[1]) / 15
-            renderScroll = (int(self.scroll[0]), int(self.scroll[1]))
-
             match self.currentPhase:
-                case 'interacting':
-                    self.Player.movable = False
-                case 'normal':
-                    self.Player.movable = True
-            
-            if self.transition < 0:
-                self.transition += 1
+                case 'finalQuiz':
+                    time_delta = self.clock.tick(60)/1000.0
 
-            self.tilemap.render(self.display, offset = renderScroll)
+                    self.display.fill((28, 138, 217))
 
-            self.Player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.Player.render(self.display, offset=renderScroll)
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit()
+                        if event.type == pygame.KEYDOWN:
+                            match event.key:
+                                case pygame.K_LEFT:
+                                    if self.currentPhase != 'interacting':
+                                        self.movement[0] = True
+                                case pygame.K_RIGHT:
+                                    if self.currentPhase != 'interacting':
+                                        self.movement[1] = True
+                                case pygame.K_a:
+                                    # self.dialogueBox.update(self.display, (self.Player.position[0] - renderScroll[0], self.Player.position[1] - renderScroll[1]))
+                                    for i in range(len(self.interactableObjects)):
+                                        lastPhase = self.currentPhase
+                                        self.currentPhase = self.interactableObjects[i].interact(self.display, renderScroll, self.dialogueBox, phase=self.currentPhase)
+                                        if self.currentPhase != lastPhase:
+                                            self.dialogueBox._setNpc_(self.interactableObjects[i].name)
+                                            break
+                        if event.type == pygame.KEYUP:
+                            match event.key:
+                                case pygame.K_LEFT:
+                                    self.movement[0] = False
+                                case pygame.K_RIGHT:
+                                    self.movement[1] = False
+                        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                            for label, btn in self.buttonsOnScreen.copy().items():
+                                if event.ui_element == btn:
+                                    self.currentPhase = self.dialogueBox.updateLines(int(label[-1]), self.buttonsOnScreen)
+                    
+                    self.guiManager.update(time_delta)
 
-            for i in range(len(self.interactableObjects)):
-                self.dialogueBox.drawable = True if self.interactableObjects[i].checkCollision(self.Player, self.display, (self.interactableObjects[i].position[0] - self.scroll[0] - self.interactableObjects[i].radius, self.interactableObjects[i].position[1] - self.scroll[1] - self.interactableObjects[i].radius)) else False
-                if self.dialogueBox.drawable:
-                    break
-            
-            for i in range(len(self.interactableObjects)):
-                self.interactableObjects[i].render(self.display, offset=renderScroll)
-                self.interactableObjects[i].animation.update()
+                    self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    match event.key:
-                        case pygame.K_LEFT:
-                            if self.currentPhase != 'interacting':
-                                self.movement[0] = True
-                        case pygame.K_RIGHT:
-                            if self.currentPhase != 'interacting':
-                                self.movement[1] = True
-                        case pygame.K_a:
-                            # self.dialogueBox.update(self.display, (self.Player.position[0] - renderScroll[0], self.Player.position[1] - renderScroll[1]))
-                            for i in range(len(self.interactableObjects)):
-                                lastPhase = self.currentPhase
-                                self.currentPhase = self.interactableObjects[i].interact(self.display, renderScroll, self.dialogueBox, phase=self.currentPhase)
-                                if self.currentPhase != lastPhase:
-                                    self.dialogueBox._setNpc_(self.interactableObjects[i].name)
-                                    break
-                if event.type == pygame.KEYUP:
-                    match event.key:
-                        case pygame.K_LEFT:
-                            self.movement[0] = False
-                        case pygame.K_RIGHT:
-                            self.movement[1] = False
-                if event.type == pygame_gui.UI_BUTTON_PRESSED:
-                    for label, btn in self.buttonsOnScreen.copy().items():
-                        if event.ui_element == btn:
-                            self.currentPhase = self.dialogueBox.updateLines(int(label[-1]), self.buttonsOnScreen)
+                    if self.dialogueBox.drawable:
+                        if self.currentPhase == 'interacting':
+                            self.screen.blit(pygame.transform.scale(self.assets['dialogueBox'], [self.screen.get_size()[0] // 2, self.screen.get_size()[1] // 3]), (self.screen.get_width() // 4, 0))
 
-                self.guiManager.process_events(event)
-            
-            self.guiManager.update(time_delta)
-            
-            if self.transition:
-                self._runTransition_()
+                        self.dialogueBox.draw(self.screen, self.Player, self.guiManager, self.buttonsOnScreen)
+                        self.guiManager.draw_ui(self.screen)
 
-            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+                    pygame.display.update()
 
-            if self.dialogueBox.drawable:
-                if self.currentPhase == 'interacting':
-                    self.screen.blit(pygame.transform.scale(self.assets['dialogueBox'], [self.screen.get_size()[0] // 2, self.screen.get_size()[1] // 3]), (self.screen.get_width() // 4, 0))
+                case _:
+                    time_delta = self.clock.tick(60)/1000.0
+                    self.display.fill((28, 138, 217))
 
-                self.dialogueBox.draw(self.screen, self.Player, self.guiManager, self.buttonsOnScreen)
-                self.guiManager.draw_ui(self.screen)
+                    self.scroll[0] += (self.Player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 20
+                    self.scroll[1] += (self.Player.rect().centery - self.display.get_height() / 1.6 - self.scroll[1]) / 15
+                    renderScroll = (int(self.scroll[0]), int(self.scroll[1]))
 
-            pygame.display.update()
+                    match self.currentPhase:
+                        case 'interacting':
+                            self.Player.movable = False
+                        case 'normal':
+                            self.Player.movable = True
+                    
+                    if self.transition < 0:
+                        self.transition += 1
+
+                    self.tilemap.render(self.display, offset = renderScroll)
+
+                    self.Player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+                    self.Player.render(self.display, offset=renderScroll)
+
+                    for i in range(len(self.interactableObjects)):
+                        self.dialogueBox.drawable = True if self.interactableObjects[i].checkCollision(self.Player, self.display, (self.interactableObjects[i].position[0] - self.scroll[0] - self.interactableObjects[i].radius, self.interactableObjects[i].position[1] - self.scroll[1] - self.interactableObjects[i].radius)) else False
+                        if self.dialogueBox.drawable:
+                            break
+                    
+                    for i in range(len(self.interactableObjects)):
+                        self.interactableObjects[i].render(self.display, offset=renderScroll)
+                        self.interactableObjects[i].animation.update()
+
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit()
+                        if event.type == pygame.KEYDOWN:
+                            match event.key:
+                                case pygame.K_LEFT:
+                                    if self.currentPhase != 'interacting':
+                                        self.movement[0] = True
+                                case pygame.K_RIGHT:
+                                    if self.currentPhase != 'interacting':
+                                        self.movement[1] = True
+                                case pygame.K_a:
+                                    # self.dialogueBox.update(self.display, (self.Player.position[0] - renderScroll[0], self.Player.position[1] - renderScroll[1]))
+                                    for i in range(len(self.interactableObjects)):
+                                        lastPhase = self.currentPhase
+                                        self.currentPhase = self.interactableObjects[i].interact(self.display, renderScroll, self.dialogueBox, phase=self.currentPhase)
+                                        if self.currentPhase != lastPhase:
+                                            self.dialogueBox._setNpc_(self.interactableObjects[i].name)
+                                            break
+                        if event.type == pygame.KEYUP:
+                            match event.key:
+                                case pygame.K_LEFT:
+                                    self.movement[0] = False
+                                case pygame.K_RIGHT:
+                                    self.movement[1] = False
+                        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                            for label, btn in self.buttonsOnScreen.copy().items():
+                                if event.ui_element == btn:
+                                    self.currentPhase = self.dialogueBox.updateLines(int(label[-1]), self.buttonsOnScreen)
+                                    interactingObject = next((obj for obj in self.interactableObjects if obj.interactable), None)
+                                    if interactingObject.name == "returnStone" and label[-1] == "1":
+                                        self.currentPhase = 'finalQuiz'
+
+                        self.guiManager.process_events(event)
+                    
+                    self.guiManager.update(time_delta)
+                    
+                    if self.transition:
+                        self._runTransition_()
+
+                    self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+
+                    if self.dialogueBox.drawable:
+                        if self.currentPhase == 'interacting':
+                            self.screen.blit(pygame.transform.scale(self.assets['dialogueBox'], [self.screen.get_size()[0] // 2, self.screen.get_size()[1] // 3]), (self.screen.get_width() // 4, 0))
+
+                        self.dialogueBox.draw(self.screen, self.Player, self.guiManager, self.buttonsOnScreen)
+                        self.guiManager.draw_ui(self.screen)
+
+                    pygame.display.update()
 
     def _runTransition_(self):
         transition_surf = pygame.Surface(self.display.get_size())
